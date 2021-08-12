770f7796cbefdef5c62e0220a732116ef5347c4d
hudehyan <65912531+hudehyan@users.noreply.github.com>
2021-08-11 22:49:01 -0400

    Update BlockList.cs

--- CP380-B1-BlockList/Models/BlockList.cs
+++ CP380-B1-BlockList/Models/BlockList.cs
@@ -25,13 +25,33 @@ namespace CP380_B1_BlockList.Models
         public void AddBlock(Block block)
         {
             // TODO
+             Block latestBlock = Chain[Chain.Count - 1];
+            block.Nonce = latestBlock.Nonce + 1;
+            block.PreviousHash = latestBlock.Hash;
+            block.Mine(this.Difficulty);
+            Chain.Add(block);
         }
 
         public bool IsValid()
         {
             // TODO
+            for (int i = 1; i < Chain.Count; i++)
+            {
+                Block c= Chain[i];
+                Block p= Chain[i - 1];
 
-            return false;
+                if (c.Hash != c.CalculateHash())
+                {
+                    return false;
+                }
+
+                if (c.PreviousHash != p.Hash)
+                {
+                    return false;
+                }
+            }
+            return true;
+
         }
     }
 }
